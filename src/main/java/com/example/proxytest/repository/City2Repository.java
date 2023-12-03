ff0355184ed8f60492529bc08c98859ba297bc4d
daejoon <kkode1911@gmail.com>
2023-12-03 14:31:59 +0900

    Added City2Repository

---
+++ src/main/java/com/example/proxytest/repository/City2Repository.java
@@ -0,0 +1,7 @@
+package com.example.proxytest.repository;
+
+import com.example.proxytest.domain.City2;
+import org.springframework.data.repository.CrudRepository;
+
+public interface City2Repository extends CrudRepository<City2, Long> {
+}
